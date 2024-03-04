0d4f4b66ebc58cc6e2842a06428c80085640f577
saurabh Pathak <91461992+saurabh9323@users.noreply.github.com>
2024-03-04 12:40:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,31 @@ const routers = require("./routes/routers.js");
 
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
-app.use(cookieParser());
+
 app.use(bodyparser.json());
-app.use(
-  cors({
-    origin: "https://attendance-ahtvwkkq6-saurabh9323.vercel.app/",
-  })
-);
+// Define the list of allowed origins
+const allowedOrigins = [
+  'https://attendance-ahtvwkkq6-saurabh9323.vercel.app',
+  'http://localhost:5173'
+];
+
+// CORS middleware function
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the request origin is in the list of allowed origins
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      // If the request origin is not in the list, use the default origin
+      callback(null, false);
+    }
+  },
+  credentials: true // Enable credentials (cookies, authorization headers) to be sent cross-origin
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions
+            app.use(cookieParser());
 
 app.use("/api", routers);
 
